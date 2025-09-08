3a0bb5c566fbf6d3555328cc53a5fd359ac4d77e
olamMat <makeitrealofi@gmail.com>
2025-09-08 09:15:41 -0600

    Update script.js

--- script.js
+++ script.js
@@ -11,8 +11,11 @@
 let dataset = [];
 let currentView = []; // <- mantiene lo que se está mostrando en la tabla
 
+
+//https://docs.google.com/spreadsheets/d/16K-i-FwK86CKryE3JFhkvsS2JOszA-Ui/edit?usp=sharing&ouid=116572784923002226502&rtpof=true&sd=true
 // Config de Google Sheets (opcional, deja vacío si no usas)
-const GOOGLE_SHEET_ID  = '1HO1dnYe55Weyswxh1qfz1Y8_5nxoGDR6'; // tu ID
+//const GOOGLE_SHEET_ID  = '1HO1dnYe55Weyswxh1qfz1Y8_5nxoGDR6'; // tu ID
+const GOOGLE_SHEET_ID  = '116572784923002226502'; // tu ID
 const GOOGLE_SHEET_GID = '0';                                   // pestaña
 
 // -------------------- Utilidades --------------------
@@ -138,7 +141,8 @@ function initPage() {
         alert('Configura GOOGLE_SHEET_ID para descargar desde Google Sheets.');
         return;
       }
-      const url = `https://drive.google.com/uc?export=download&id=1HO1dnYe55Weyswxh1qfz1Y8_5nxoGDR6`;
+     // const url = `https://drive.google.com/uc?export=download&id=1HO1dnYe55Weyswxh1qfz1Y8_5nxoGDR6`;
+       const url = `https://drive.google.com/uc?export=download&id=116572784923002226502`;
       window.open(url, '_blank');
     });
   }
@@ -301,3 +305,4 @@ function toNumber(val) {
 
 // -------------------- Arranque --------------------
 document.addEventListener('DOMContentLoaded', loadDataset);
+
